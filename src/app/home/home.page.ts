a87fa5c2e6b1bdf380009c26cd9cff7e46756f79
Sarias <53831285s@iespoblenou.org>
2022-01-05 19:33:53 +0100

    tourus and fortmatic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import Web3 from 'web3';
+import Torus from '@toruslabs/torus-embed';
+import Fortmatic from 'fortmatic';
+import {AbstractProvider} from "web3-core";
 
 @Component({
   selector: 'app-home',
@@ -7,6 +11,29 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  web3 = null;
+  formaticProvider = new Fortmatic('pk_live_13D46A6E33995083');
+
   constructor() {}
 
+  async openTorus() {
+    const torus = new Torus();
+    await torus.init({
+      buildEnv: 'development', // default: production
+      enableLogging: true,
+      network: {
+        host: 'mumbai', // optional
+        chainId: 80001, // optional
+      },
+      showTorusButton: true // default: true
+    });
+    await torus.ethereum.enable();
+    this.web3 = new Web3(torus.provider as AbstractProvider);
+  }
+
+  async openFortmatic() {
+    const fortmatic = this.formaticProvider;
+    this.web3 = new Web3(fortmatic.getProvider() as unknown as AbstractProvider);
+    this.web3.currentProvider.enable();
+  }
 }
